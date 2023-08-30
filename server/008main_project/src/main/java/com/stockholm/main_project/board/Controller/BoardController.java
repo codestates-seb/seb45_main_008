12abf8a06911f10faa5193ca0f56ea1d2ffb5800
이준석 <cha14678@naver.com>
2023-08-30 14:02:03 +0900

    Feat : 게시판 기능 구현

---
+++ server/008main_project/src/main/java/com/stockholm/main_project/board/Controller/BoardController.java
@@ -0,0 +1,82 @@
+package com.stockholm.main_project.board.Controller;
+
+import com.stockholm.main_project.board.Service.BoardService;
+import com.stockholm.main_project.board.entity.Board; // Board 클래스를 참조하도록 수정
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/boards")
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    // 게시물 생성
+    @PostMapping
+    public ResponseEntity<Board> createPost(@RequestBody @Valid Board board) {
+        Board createdBoard = boardService.createBoard(board);
+        return new ResponseEntity<>(createdBoard, HttpStatus.CREATED);
+    }
+
+    // 모든 게시물 조회
+    @GetMapping
+    public ResponseEntity<List<Board>> getAllBoards() {
+        List<Board> boards = boardService.getAllBoards();
+        return new ResponseEntity<>(boards, HttpStatus.OK);
+    }
+
+    // 특정 게시물 조회
+    @GetMapping("/{boardId}")
+    public ResponseEntity<Board> getBoardById(@PathVariable Long boardId) {
+        Board board = boardService.getBoardById(boardId);
+        if (board != null) {
+            return new ResponseEntity<>(board, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // 게시물 업데이트
+    @PutMapping("/{boardId}")
+    public ResponseEntity<Board> updateBoard(@PathVariable Long boardId, @RequestBody @Valid Board board) {
+        Board updatedBoard = boardService.updateBoard(boardId, board);
+        if (updatedBoard != null) {
+            return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    //게시물 수정
+    @PatchMapping("/{boardId}")
+    public ResponseEntity<Board> patchBoard(
+            @PathVariable Long boardId,
+            @RequestBody Map<String, Object> updates) {
+
+        Board updatedBoard = boardService.patchBoard(boardId, updates);
+
+        if (updatedBoard != null) {
+            return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    // 게시물 삭제
+    @DeleteMapping("/{boardId}") // 변수 이름도 수정
+    public ResponseEntity<Void> deleteBoard(@PathVariable Long boardId) { // 변수 이름도 수정
+        boolean deleted = boardService.deleteBoard(boardId); // 서비스 메서드도 수정
+        if (deleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ server/008main_project/src/main/java/com/stockholm/main_project/board/dto/BoardDto.java
@@ -0,0 +1,11 @@
+package com.stockholm.main_project.board.dto;
+
+public class BoardDto {
+    private long id;
+    private String title;
+    private String body;
+    private String img;
+    private long memberId;
+    private long commentId;
+
+}
