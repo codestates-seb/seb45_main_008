3589a7a265f581bb4b256caf8ae46d379da3cf61
novice1993 <novice1993@gmail.com>
2023-08-30 15:35:33 +0900

    [Feat] 헤더 완성으로 인한 브랜치 병합
    - 메인 페이지 상단에 들어갈 헤더 컴포넌트 완성으로 인한 브랜치 병합
    
    Issues #11

--- client/src/page/MainPage.tsx
+++ client/src/page/MainPage.tsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import Header from "../components/header"
+import Header from "../components/header";
 
 import CentralChartSection from "../components/CentralChartSection/Index";
 
