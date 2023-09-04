20fa8202d01c53b57b14b56a274366b0d43dc9a2
이준석 <cha14678@naver.com>
2023-09-04 21:26:58 +0900

    Feat : Board swagger적용

--- server/008main_project/src/main/java/com/stockholm/main_project/board/dto/BoardDto.java
+++ server/008main_project/src/main/java/com/stockholm/main_project/board/dto/BoardDto.java
@@ -1,11 +1,25 @@
 package com.stockholm.main_project.board.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class BoardDto {
+    @Schema(description = "게시물 ID", example = "1")
     private long id;
+
+    @Schema(description = "게시물 제목", defaultValue = "제목 없음")
     private String title;
+
+    @Schema(description = "게시물 내용", defaultValue = "내용 없음")
     private String body;
+
+    @Schema(description = "이미지 URL")
     private String img;
+
+    @Schema(description = "회원 ID", example = "101")
     private long memberId;
+
+    @Schema(description = "댓글 ID", example = "201")
     private long commentId;
 
+    // Getter 및 Setter 메서드 생략
 }
