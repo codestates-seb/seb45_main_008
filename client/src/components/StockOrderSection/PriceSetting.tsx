87e77c81e4237cddbaed9fb74da8d2e97929524e
novice1993 <novice1993@gmail.com>
2023-09-08 01:28:27 +0900

    [Remove] 사용하지 않는 코드 주석처리
    - 지정가/시장가 관련 기능 활용하지 않는 것으로 계획이 수정되어 관련된 코드 주석처리
    - 진행 상황 고려하여 재사용 하거나, 사용하지 않기로 확정될 경우 주석 처리한 부분 및 기타 코드 (ex. redux-toolkit 관련 reducer 등) 삭제 예정
    
    Issues #17

--- client/src/components/StockOrderSection/PriceSetting.tsx
+++ client/src/components/StockOrderSection/PriceSetting.tsx
@@ -1,27 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import { styled } from "styled-components";
-import { setSpecifiedPrice, setMarketPrice } from "../../reducer/StockPriceType-Reducer";
 import { plusStockOrderPrice, minusStockOrderPrice } from "../../reducer/StockOrderPrice-Reducer";
 import { StateProps } from "../../models/stateProps";
 
 const priceSettingTitle: string = "가격";
-const specifiedPriceBtnText: string = "지정가";
-const marketPriceBtnText: string = "시장가";
 const unitText: string = "원";
 
 const PriceSetting = () => {
-  const priceType = useSelector((state: StateProps) => state.stockPriceType);
   const orderPrice = useSelector((state: StateProps) => state.stockOrderPrice);
   const dispatch = useDispatch();
-
-  // 시장가, 지정가 변경
-  const handleSetSepcifiedPrice = () => {
-    dispatch(setSpecifiedPrice());
-  };
-
-  const handleSetMarketPrice = () => {
-    dispatch(setMarketPrice());
-  };
 
   // 거래가 증가/감소
   const handlePlusOrderPrice = () => {
@@ -36,14 +23,6 @@ const PriceSetting = () => {
     <Container>
       <div className="PriceCategoryBox">
         <div className="Title">{priceSettingTitle}</div>
-        <div className="ButtonContainer">
-          <SepcifiedPriceBtn onClick={handleSetSepcifiedPrice} priceType={priceType}>
-            {specifiedPriceBtnText}
-          </SepcifiedPriceBtn>
-          <MarketPriceBtn onClick={handleSetMarketPrice} priceType={priceType}>
-            {marketPriceBtnText}
-          </MarketPriceBtn>
-        </div>
       </div>
       <div className="PriceSettingBox">
         <PriceController defaultValue={orderPrice} value={orderPrice} />
@@ -62,11 +41,6 @@ const PriceSetting = () => {
 };
 
 export default PriceSetting;
-
-// type 정의
-interface PriceTypeProps {
-  priceType: boolean;
-}
 
 // component 생성
 const Container = styled.div`
@@ -131,29 +105,6 @@ const Container = styled.div`
   }
 `;
 
-const SepcifiedPriceBtn = styled.button<PriceTypeProps>`
-  width: 46px;
-  height: 21px;
-  border: none;
-  box-shadow: ${(props) => !props.priceType && "0.7px 0.7px 3px rgba(0, 0, 0, 0.4);"};
-  border-radius: 0.3rem;
-  background-color: ${(props) => (props.priceType ? "f2f2f2" : "white")};
-  color: ${(props) => (props.priceType ? "#999999" : "black")};
-
-  font-size: 13px;
-`;
-
-const MarketPriceBtn = styled.button<PriceTypeProps>`
-  width: 46px;
-  height: 21px;
-  border: none;
-  border-radius: 0.3rem;
-  box-shadow: ${(props) => props.priceType && "0.7px 0.7px 3px rgba(0, 0, 0, 0.4);"};
-  background-color: ${(props) => (props.priceType ? "white" : "f2f2f2")};
-  color: ${(props) => (props.priceType ? "black" : "#999999")};
-  font-size: 13px;
-`;
-
 const PriceController = styled.input`
   flex: 1 0 0;
   height: 30px;
@@ -179,3 +130,57 @@ const UnitContent = styled.div`
   border-bottom: 1px solid darkgray;
   background-color: #ffffff;
 `;
+
+// 지정가, 시장가 관련된 코드
+
+// const specifiedPriceBtnText: string = "지정가";
+// const marketPriceBtnText: string = "시장가";
+// const priceType = useSelector((state: StateProps) => state.stockPriceType);
+
+// 시장가, 지정가 변경
+// const handleSetSepcifiedPrice = () => {
+//   dispatch(setSpecifiedPrice());
+// };
+
+// const handleSetMarketPrice = () => {
+//   dispatch(setMarketPrice());
+// };
+
+{
+  /* <div className="ButtonContainer">
+          <SepcifiedPriceBtn onClick={handleSetSepcifiedPrice} priceType={priceType}>
+            {specifiedPriceBtnText}
+          </SepcifiedPriceBtn>
+          <MarketPriceBtn onClick={handleSetMarketPrice} priceType={priceType}>
+            {marketPriceBtnText}
+          </MarketPriceBtn>
+        </div> */
+}
+
+// // type 정의
+// interface PriceTypeProps {
+//   priceType: boolean;
+// }
+
+// const SepcifiedPriceBtn = styled.button<PriceTypeProps>`
+//   width: 46px;
+//   height: 21px;
+//   border: none;
+//   box-shadow: ${(props) => !props.priceType && "0.7px 0.7px 3px rgba(0, 0, 0, 0.4);"};
+//   border-radius: 0.3rem;
+//   background-color: ${(props) => (props.priceType ? "f2f2f2" : "white")};
+//   color: ${(props) => (props.priceType ? "#999999" : "black")};
+
+//   font-size: 13px;
+// `;
+
+// const MarketPriceBtn = styled.button<PriceTypeProps>`
+//   width: 46px;
+//   height: 21px;
+//   border: none;
+//   border-radius: 0.3rem;
+//   box-shadow: ${(props) => props.priceType && "0.7px 0.7px 3px rgba(0, 0, 0, 0.4);"};
+//   background-color: ${(props) => (props.priceType ? "white" : "f2f2f2")};
+//   color: ${(props) => (props.priceType ? "black" : "#999999")};
+//   font-size: 13px;
+// `;
