bfc69012b76ee902f476025bd2fbde0416fa41f3
novice1993 <novice1993@gmail.com>
2023-09-09 01:30:17 +0900

    [Remove] 가격지정 관련 파일/코드 삭제
    - 지정가/시장가 선택 기능 제거로 인한 관련 파일/코드 삭제
    
    Issues #17

--- client/src/store/config.ts
+++ client/src/store/config.ts
@@ -1,16 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { stockOrderTypeReducer } from "../reducer/StockOrderType-Reducer";
-import { stockPriceTypeReducer } from "../reducer/StockPriceType-Reducer";
 import { stockOrderPriceReducer } from "../reducer/StockOrderPrice-Reducer";
 import { expandScreenReducer } from "../reducer/ExpandScreen-Reducer";
 import { stockOrderSetReducer } from "../reducer/StockOrderSet-Reducer";
 import { companyIdReducer } from "../reducer/CompanyId-Reducer";
-import  memberInfoReducer from '../reducer/member/memberInfoSlice';
+import memberInfoReducer from "../reducer/member/memberInfoSlice";
 
 const store = configureStore({
   reducer: {
     stockOrderType: stockOrderTypeReducer,
-    stockPriceType: stockPriceTypeReducer,
     stockOrderPrice: stockOrderPriceReducer,
     expandScreen: expandScreenReducer,
     stockOrderSet: stockOrderSetReducer,
