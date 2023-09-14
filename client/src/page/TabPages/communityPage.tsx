a8c3226a64b9c4822369546edc053351d94a53f3
sinjw <nadasjw@gmail.com>
2023-09-14 10:33:08 +0900

    [Fix]레이아웃 추가 수정 -각페이지에 추가적으로 높이 고정 Issues #10

--- client/src/page/TabPages/communityPage.tsx
+++ client/src/page/TabPages/communityPage.tsx
@@ -1,15 +1,19 @@
 //import { TabNavArea } from "./TabNavArea";
 
 import TimeLineComponent from "../../components/communityComponents/index";
-
+import styled from "styled-components";
 export const Community = () => {
   // const [submitButton, setSubmitButton] = useState("");
 
   // 커뮤니티 페이지 텍스트문구 객체 정리
 
   return (
-    <div>
+    <CommunityContainer>
       <TimeLineComponent />
-    </div>
+    </CommunityContainer>
   );
 };
+
+const CommunityContainer = styled.div`
+  max-height: 500px;
+`;
