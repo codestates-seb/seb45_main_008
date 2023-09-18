dffef482fe5303e05a5477fb26a4459907c26b80
rkddnr <rkddnr05@naver.com>
2023-09-18 16:51:33 +0900

    FIX: member 테이블 제거 시 stockorder자동 제거 추가

--- server/008main_project/src/main/java/com/stockholm/main_project/stock/repository/StockOrderRepository.java
+++ server/008main_project/src/main/java/com/stockholm/main_project/stock/repository/StockOrderRepository.java
@@ -11,6 +11,7 @@ public interface StockOrderRepository extends JpaRepository<StockOrder, Long> {
     List<StockOrder> findAllByMember_MemberIdOrderByModifiedAtDesc(long memberId);
     List<StockOrder> findAllByMember_MemberIdAndCompany_CompanyIdAndOrderStatesAndOrderTypes(long memberId, long companyId, StockOrder.OrderStates orderStates, StockOrder.OrderTypes orderTypes);
 
+    List<StockOrder> findByMemberMemberId(long memberId);
     // MEMBER_ID로 주식 주문을 모두 삭제하는 JPQL 쿼리
 //    @Modifying
 //    @Query("DELETE FROM StockOrder so WHERE so.memberId = :memberId")
