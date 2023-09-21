92ed288ff5dd34c90e19dafec92cd206ea49dc6b
novice1993 <novice1993@gmail.com>
2023-09-22 02:34:03 +0900

    [Fix] 함수 설명관련 오타 수정
    - 자동 로그아웃 함수 설명 관련 오타 수정
    
    Issues #122

--- client/src/utils/setAutoLogoutAlarm.ts
+++ client/src/utils/setAutoLogoutAlarm.ts
@@ -2,14 +2,14 @@ import { toast } from "react-toastify";
 import { setLogoutState } from "../reducer/member/loginSlice";
 
 /*
-- 자동 로그아웃 설정을 하는 Custom Hook입니다.
+- 자동 로그아웃 설정을 하는 함수입니다.
 
 - 총 3차례의 토스트 메시지 안내를 실행합니다
   1차) 로그인 시 30분 뒤 로그아웃 됨을 알림
   2차) 로그아웃 시간 1분 남음을 알림
   3차) 로그아웃 처리 되었음을 알림
 
-- 해당 hook은 총 4개의 인자를 가집니다
+- 해당 함수는 총 4개의 인자를 가집니다
   1) 로그인 전역상태 변경에 필요한 dispatch
   2) 알람 설정 개수 (first일 경우 3개 다, second일 경우 2개, last일 경우 마지막 1개만 설정)
   3) 1차 알림 설정 시간 (설정한 시간만큼 지난 후 1차 알림 발생)
